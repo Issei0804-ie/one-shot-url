0489c479c202a2e8c2f0ba91fe85e6568db94f70
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2022-05-17 18:12:52 +0900

    bind するポート番号を環境変数から指定できるように変更

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"log"
 	"one-shot-url/api"
 	"one-shot-url/database"
 	"one-shot-url/short"
 	"one-shot-url/util"
+	"os"
+	"strconv"
 )
 
 func main() {
 	util.InitEnv()
 	util.InitLog()
+	rawPort := os.Getenv("PORT")
+	port, err := strconv.Atoi(rawPort)
+	if err != nil {
+		log.Fatalln("can not get a port number that environment value. did you modify .env?")
+	}
 
 	db := database.NewDB(false)
 	s := short.NewShort(db.IsExistShortUrl)
 	api := api.NewAPI(s, db)
-	api.Run(8080)
+	api.Run(port)
 }
