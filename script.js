53ca1c63cf52e2b6accb58d6891fd3c29716da52
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2022-05-24 14:41:36 +0900

    負荷シナリオの修正

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import http from "k6/http";
+import {time} from "k6";
 
 export default function() {
     let data  = {url:"https:/exmaple.com"};
     let response = http.post("http://localhost:8080/short", JSON.stringify(data));
+    time(1);
 };
